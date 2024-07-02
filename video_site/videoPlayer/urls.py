0899492ff3678571f2ef2698414e55656aff34d3
abtin <aangoorani@gmail.com>
2024-07-02 23:05:34 +0330

    feat: done

--- video_site/videoPlayer/urls.py
+++ video_site/videoPlayer/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home_page'),
     path('submit-comment/<str:uri>', views.submit_comment, name='submit_comment'),
+    # path('get-summary', views.extra),
     path('<str:uri>/', views.show, name='video_page'),
 ]
--- video_site/videoPlayer/views.py
+++ video_site/videoPlayer/views.py
@@ -2,74 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from .models import Video, Comment, Thumbnail
 from django.urls import reverse
-
+from .utils.aiAgent import comment_analyzer, summary_generator
 
 def index(request):
     return render(request, 'videoPlayer/index.html', {
         'thumbnails': Thumbnail.objects.all()
     })
-
-#
-# def show(request, uri:str):
-#     encoded_uri = uri[:]
-#     uri = uri.replace("__", '/')
-#
-#     if request.method == 'POST':
-#         submit_comment(request, uri)
-#         # HttpResponseRedirect(reverse('video_page', args=(encoded_uri,)))
-#
-#     video = Video.objects.filter(uri=uri).first()
-#
-#     # Filter comments related to the specific video and order them by timestamp
-#     comments = Comment.objects.filter(video=video).order_by('-time_stamp')
-#     # print(comments)
-#
-#     # Render the template with the video URI and comments
-#     if video.playback_type == 'local':
-#         return render(request, 'videoPlayer/video.html', {
-#             'uri': uri,
-#             'comments': comments,
-#             'encoded_uri': encoded_uri,
-#         })
-#
-#     elif video.playback_type == 'dash':
-#         return render(request, 'videoPlayer/dash_video.html', {
-#             'uri': uri,
-#             'comments': comments,
-#             'encoded_uri': encoded_uri,
-#         })
-#     elif video.playback_type == 'cdn1':
-#         return render(request, 'videoPlayer/cdn_dash.html', {
-#             'uri': uri,
-#             'comments': comments,
-#             'encoded_uri': encoded_uri,
-#         })
-#     elif video.playback_type == 'cdn2':
-#         return render(request, 'videoPlayer/cdn_hls.html', {
-#             'uri': uri,
-#             'comments': comments,
-#             'encoded_uri': encoded_uri,
-#         })
-#     else:
-#         return HttpResponse("NOT Implemented")
-#
-# def submit_comment(request, uri: str):
-#     if request.method == 'POST':
-#         encoded_uri = uri[:]
-#         uri = uri.replace('__', '/')
-#         video = Video.objects.filter(uri=uri).first()
-#         name = request.POST.get('name')
-#         if not name: name = 'John Doe'
-#         text = request.POST.get('new_comment')
-#         print(request.POST)
-#
-#         # Create a new Comment object and save it to the database
-#         comment = Comment(video=video, name=name, text=text, sentiment='default')
-#         comment.save()
-#
-#         # Return a success response (could be a redirect or a JSON response)
-#         return JsonResponse({'message': 'Comment submitted successfully!'}, status=200)
-
 
 
 def show(request, uri: str):
@@ -112,7 +50,25 @@ def submit_comment(request, uri: str):
 
         # Create a new Comment object and save it to the database
         comment = Comment(video=video, name=name, text=text, sentiment='default')
+        sentiment = comment_analyzer.analyze(text)
+        comment.sentiment = sentiment
         comment.save()
 
         # Return a success response (JSON response)
         return JsonResponse({'message': 'Comment submitted successfully!'}, status=200)
+
+
+# for requesting to generate summary, uncomment when needed, also uncomment the relevant path in urls.py
+# def extra(request):
+#     thumbnails = Thumbnail.objects.all()
+#     i=0
+#     for thumbnail in thumbnails:
+#         print(thumbnail)
+#         yt_link = thumbnail.video.yt_link
+#         summary = summary_generator.summarize_youtube_video(yt_link)
+#         thumbnail.summary = summary
+#         thumbnail.save(update_fields=['summary'])
+#         i+=1
+#         print('done:', i)
+#
+#     return HttpResponse('Done')
